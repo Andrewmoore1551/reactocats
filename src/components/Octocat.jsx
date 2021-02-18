55b73475d12de94c25a022ea83d77ed76d6f6a7b
Andrew Moore <mooreandrew015@gmail.com>
2021-02-17 19:39:53 -0500

    completed

---
+++ src/components/Octocat.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+export class Octocat extends React.Component {
+  render() {
+ 
+    return <article>
+    <a href={this.props.href}
+      ><img
+        src={this.props.src}
+        width="400"
+        height="400"
+        alt={this.props.name}
+        class="bigimg"
+      />
+    </a>
+    <ul>
+      <li>
+        #143:
+        <a href={this.props.href}
+          ><strong>{this.props.name}</strong></a
+        >
+      </li>
+      <li>
+        <a href={this.props.littlehref}
+          ><img
+            src={this.props.littlesrc}
+            width="24px"
+            height="24px"
+            alt={this.props.littlealt}
+            class="littleimg"
+          />
+        </a>
+      </li>
+    </ul>
+  </article>
+
+ 
+  }
+}
+
